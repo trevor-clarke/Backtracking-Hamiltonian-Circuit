29d1ecf2f366b776545c15f198e905fc4208547f
Trevor Clarke <trevor.clarke@ryerson.ca>
2020-03-08 22:30:04 -0400

    Updated the code, added a class to make it work standalone.

--- Graph.py
+++ Graph.py
@@ -1,25 +1,55 @@
-    def getHamiltonian(self):
-        if self.isConnected():
-            walk = []
-            walk.append(0)
-            if self.tryVisiting(0, walk) or (self.totalV == 1):
-                return walk
-        return None
+class Graph:
 
-    def tryVisiting(self, vertex, Hamiltonian):
-        hamSplit = str(Hamiltonian).split()
-        hamLen = len(hamSplit)
-        if (vertex == 0) and (hamLen > 1):
-            return (hamLen == self.totalV+1)
-        else:
-            for newVertex in range(0,self.totalV):
-                distance = self.getEdgeCount(vertex, newVertex)
-                isRepeat = str(newVertex) in (hamSplit[1:]+[vertex])
-                isPrevious = (newVertex == vertex)
-                if (distance == 1) and (isRepeat == False) and (isPrevious == False):
-                    Hamiltonian.addVertex(newVertex)
-                    if self.tryVisiting(newVertex, Hamiltonian):
-                        return True
-                    else:
-                        Hamiltonian.removeLastVertex()
-            return False
+  adjacency_matrix = []
+  totalV = 0
+
+  def __init__(self, adjacency_matrix):
+    self.adjacency_matrix = adjacency_matrix
+    self.totalV = len(self.adjacency_matrix)
+
+  @classmethod
+  def fromFile(self, filename):
+    f = open(filename, "r")
+    with f as lines:
+      adjacency_matrix = [[int(i) for i in line.split()] for line in lines]
+    f.close()
+    return Graph(adjacency_matrix)
+    
+  def isDistanceOne(self, vertex, newVertex):
+    return self.adjacency_matrix[vertex][newVertex] == 1
+
+  def getHamiltonian(self):
+    walk = []
+    walk.append(0)
+    if self.tryVisiting(walk) or (self.totalV ==1):
+        return walk
+    return None
+
+  def tryVisiting(self, Hamiltonian):
+    vertex = Hamiltonian[-1]
+    hamLen = len(Hamiltonian)
+    if (vertex == 0) and (hamLen > 1):
+        return (hamLen == self.totalV+1)
+    else:
+        for newVertex in range(0,self.totalV):
+            distance = self.isDistanceOne(vertex, newVertex)
+            isRepeat = newVertex in (Hamiltonian[1:]+[vertex])
+            isPrevious = (newVertex == vertex)
+            if (distance == 1) and (isRepeat == False) and (isPrevious == False):
+                Hamiltonian.append(newVertex)
+                if self.tryVisiting(Hamiltonian):
+                    return True
+                else:
+                    Hamiltonian.pop()
+        return False
+
+test = Graph([[0, 1, 1],[1,0,1],[1,1,0]])
+print(test.getHamiltonian())
+
+test2 = Graph([[0,1,0,1],[1,0,1,1],[0,1,0,1],[1,1,1,0]])
+print(test2.getHamiltonian())
+
+
+test3 = Graph.fromFile("test")
+print(test3.getHamiltonian())
+
